f666403e4c9ca1ba3d8a29dc771f35b45bab707d
Travis <travis3214@yahoo.com>
2023-07-27 23:54:00 -0400

    made the write to file function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // Const for the shapes that are going to be used from shapes.js //
-const {square, circle, triangle} = require('./lib/shapes')
+const {Square, Circle, Triangle} = require('./lib/shapes')
 
 // Creating inquirer prompt for the user to input what they want their logo to be //
 function inqPrompt() {
@@ -22,7 +22,7 @@ inquirer.prompt([
         type:'list',
         name:'shape',
         message:'What shape would you like to use for your image:',
-        choices: ['square', 'cirlce', 'triangle'],
+        choices: ['Square', 'Circle', 'Triangle'],
     },
     {
         type:'input',
@@ -39,3 +39,44 @@ inquirer.prompt([
       }
     })
 };
+
+// Making a function to write the svg logo using the users answers //
+function writeToFile(fileName, answers) {
+    // This will make sure the file starts empty //
+    let svgString = "";
+    // Sets dimensions of logo container //
+    svgString =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    // This wraps the users text in a <g> tag so the input text shows up in front instead of behind the logo //
+    svgString += "<g>";
+    // Takes users selected shape and inputs it into the svg //
+    svgString += `${answers.shape}`;
+
+    // Conditional check to add users shape choice to the svg //
+    let shapeChoice;
+    if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        svgString += `<rect x="73" y="40" height="160" width="160"  fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "Circle") {
+        shapeChoice = new Circle();
+        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
+    } else {
+        shapeChoice = new Triangle();
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+    }
+
+    // <text> tag gives the text properties i want to the users text //
+    svgString += `<text x="150" y="130" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>`;
+    // Closing </g> tag //
+    svgString += "</g>";
+    // Closing </svg> tag //
+    svgString += "</svg>";
+
+    // Use fs to generate svg file //
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Successfully generated logo.svg");
+    });
+}
+
+// Calls the inquirer prompt when node index.js is ran //
+inqPrompt();
